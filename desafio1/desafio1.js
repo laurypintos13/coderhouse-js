8b58138dad2af7d86c2ddbf84b1353b142080a94
laurypintos13 <laurypintos.13@gmail.com>
2022-01-17 23:07:39 -0300

    extra desafio

--- desafio1/desafio1.js
+++ desafio1/desafio1.js
@@ -8,7 +8,6 @@ function calcularRecargo(precio) {
 let continuar = true;
 
 while (continuar) {
-
     let precio = parseInt(prompt("Ingrese el valor del producto"));
     let tipoPago =  prompt("Indique: efectivo - tarjeta").toLowerCase();   
     const totalDescuento = calcularDescuento(precio);
@@ -27,3 +26,32 @@ while (continuar) {
     continuar = prompt("Desea continuar? ingrese si o no").toLowerCase() === "si";
 }
 
+
+//OTRA MANERA DE REALIZARLO...
+
+function procesoPago(precio, tipoPago) {
+    
+    const totalDescuento = precio - (precio * 0.1);
+    const totalRecargo = precio - (precio * 0.05);
+        
+    if (tipoPago === "efectivo") {           
+        alert("El valor a pagar con descuento es " + totalDescuento + " pesos.");
+    } else if (tipoPago === "tarjeta") {        
+        alert("El valor a pagar con recargo es " + totalRecargo + " pesos.");
+        let cantCuotas = parseInt(prompt("cantidad de cuotas?"));
+        alert("Debera abonar: " + cantCuotas + " cuotas de " + (totalRecargo/cantCuotas) + " pesos.");
+    } else {
+        alert("La palabra ingresada es incorrecta");
+    }
+}
+
+let continuar = true;
+
+while (continuar) {
+    let precio = parseInt(prompt("Ingrese el valor del producto"));
+    let tipoPago =  prompt("Indique: efectivo - tarjeta").toLowerCase();
+
+    procesoPago(precio, tipoPago);
+
+    continuar = prompt("Desea continuar? ingrese si o no").toLowerCase() === "si";
+}
